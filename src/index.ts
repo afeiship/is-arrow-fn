f78a00c90327bfb6e0aab5fbb8f590d683495cd3
afeiship <1290657123@qq.com>
2023-07-31 09:41:25 +0800

    refactor: update impl

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ declare var wx: any;
 const FN_RE = /^([^{=]+|\(.*\)\s*)?=>/;
 const BLANK_RE = /\s/;
 
-const isArrowFn = (f) => typeof f === 'function' && FN_RE.test(f.toString().replace(BLANK_RE, ''));
+const isArrowFn = (f) => {
+  if (typeof f !== 'function') return false;
+  return FN_RE.test(f.toString().replace(BLANK_RE, ''));
+};
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
